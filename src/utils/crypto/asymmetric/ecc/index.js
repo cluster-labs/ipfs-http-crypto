fd4070bad6e3a35e790cfaa798405451a5414fcf
vasa <vasa@towardsblockchain.com>
2019-04-22 02:02:17 +0530

    updated ecc util

--- src/utils/crypto/asymmetric/ecc/index.js
+++ src/utils/crypto/asymmetric/ecc/index.js
@@ -1,5 +1,6 @@
-var eccrypto = require("eccrypto");
+var eccrypto = require("eccrypto")
 var crypto = require('crypto')
+var keccak = require('keccak')
 
 const encrypt = (publicKey, data, callback) => {
     eccrypto.encrypt(Buffer.from(publicKey, 'hex'), Buffer.from(data, 'utf8')).then(encrypted => {
@@ -38,12 +39,27 @@ const getPublicByPrivate = privateKey => {
     return eccrypto.getPublic(privateKey)
 }
 
+const compressPublicKey = publicKey => {
+    return '04'+publicKey
+}
+
+const decompressPublicKey = publicKey => {
+    return publicKey.slice(2)
+}
+
 const generateBrainWallet = entropy => {
     let privateKey = Buffer.from(crypto.createHash('sha256').update(entropy).digest('hex'), 'hex')
+    let publicKey = getPublicByPrivate(privateKey).toString('hex')
     return {
         privateKey: privateKey.toString('hex'),
-        publicKey: getPublicByPrivate(privateKey).toString('hex')
+        publicKey: publicKey,
+        address: getAddressFromPublicKey(publicKey)
     }
+}
+
+const getAddressFromPublicKey = publicKey => {
+    return '0x'+keccak('keccak256').update(Buffer.from(publicKey.slice(2), 'hex'))
+    .digest().slice(-20).toString('hex')
 }
 
 module.exports = {
@@ -51,5 +67,6 @@ module.exports = {
     decrypt,
     generateKeyPair,
     getPublicByPrivate,
-    generateBrainWallet
+    generateBrainWallet,
+    getAddressFromPublicKey
 }
